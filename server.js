9f8602d5b53f65f246268062444cd59a53f15ab5
Ryan Calingo <gryan.calingo@gmail.com>
2018-03-18 03:02:09 -0400

    set express server

---
+++ server.js
@@ -0,0 +1,66 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+// Our scraping tools
+// Axios is a promised-based http library, similar to jQuery's Ajax method
+// It works on the client and on the server
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+// Require all models
+// var db = require("./models");
+
+var PORT = 3000;
+
+// Initialize Express
+var app = express();
+
+// Configure middleware
+try {
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Use body-parser for handling form submissions
+app.use(bodyParser.urlencoded({ extended: true }));
+// Use express.static to serve the public folder as a static directory
+app.use(express.static("public"));
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+// By default mongoose uses callbacks for async queries, we're setting it to use promises (.then syntax) instead
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+
+// Routes
+
+// parse application/json
+app.use(bodyParser.json());
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("hbs", exphbs({
+  defaultLayout: "main",
+  extname: '.hbs'
+}));
+app.set("view engine", ".hbs");
+
+// Import routes and give the server access to them.
+// var routes = require("./controllers/burgers_controller.js");
+
+// app.use('/', routes);
+
+app.get('/', function (req, res) {
+  res.render('home');
+});
+}
+catch(err) {
+  console.log(err);
+} 
+
+// Start the server
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
+});
